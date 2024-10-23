629bcecad5b183835a342e5d6e3fe58e3be39a55
Leonardo Bitencourt Marques <leonardo.marques@dbserver.com.br>
2024-10-23 17:47:09 -0300

    feature:card 123 ok

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> editado
+          card 123 completo
         </p>
         <a
           className="App-link"
